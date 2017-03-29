8507c5c6359a948bab81aa32788e27c30842299f
Peter Szujewski <pszujewski@gmail.com>
2017-03-29 17:05:14 -0400

    request body will be sent

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/', jsonParser, (req, res) => {
   const newItem = req.body.title;
   knex.insert({item: newItem}).into('items')
   .then(result => {
-    return res.status(201).json({title: newItem});
+    return res.status(201).json(req.body);
   })
   .catch(error => { console.log(error.stack) });
 });
