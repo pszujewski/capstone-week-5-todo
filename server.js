a5ba315065ca86bcf8e47631a02f7ba9d2674e6e
Peter Szujewski <pszujewski@gmail.com>
2017-03-29 17:22:30 -0400

    Headers adapted

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
