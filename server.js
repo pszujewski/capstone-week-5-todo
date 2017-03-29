5723be1446718b884a38ae162b6ac037c3841da9
Peter Szujewski <pszujewski@gmail.com>
2017-03-29 14:45:23 -0400

    ready to test cors headers

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
-var app = express();
+const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -12,6 +14,10 @@ app.use('/', express.static('public'));
 
 app.get('/hi', (req, res) => {
   res.send("Yoyoyoyo");
+});
+
+app.post('/', jsonParser, (req, res) => {
+  res.send('post request');
 });
 
 app.listen(process.env.PORT || 8080);
