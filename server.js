c39b1651a2921d2f19d81d94e02381ac2eb6ec2e
Peter Szujewski <pszujewski@gmail.com>
2017-03-29 14:33:03 -0400

    added root endpoint

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-app.use(express.static('public'));
+// a change
+app.use('/', express.static('public'));
 
 app.get('/hi', (req, res) => {
   res.send("Yoyoyoyo");
