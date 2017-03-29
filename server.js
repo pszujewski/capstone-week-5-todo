fb6867dd948865988702227bc48d2bb10dba4cd1
Peter Szujewski <pszujewski@gmail.com>
2017-03-29 16:55:21 -0400

    as json

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/', jsonParser, (req, res) => {
   const newItem = req.body.title;
   knex.insert({item: newItem}).into('items')
   .then(result => {
-    return res.status(201).send(newItem);
+    return res.status(201).json({title: newItem});
   })
   .catch(error => { console.log(error.stack) });
 });
