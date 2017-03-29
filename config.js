653abbdc9f54172374502a0810fa105f50caf5c1
Peter Szujewski <pszujewski@gmail.com>
2017-03-29 16:39:26 -0400

    second attempt of post test

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const DEV = {
               client: 'pg',
               // ElephantSQL Config
-              // connection:'postgres://qvddfhyu:zyxUNHQzhz0Vd6J0lM@stampy.db.elephantsql.com:5432/qvddfhyu'
-              connection: {
-                  user: 'dev',
-                  //password: 'letmein',
-                  database: 'dev-todo-list'
-              },
+              connection:'postgres://unynwzad:l7b53MszMdh4rWvrhodz8EbXYAEqzdui@stampy.db.elephantsql.com:5432/unynwzad'
+            //   connection: {
+            //       user: 'dev',
+            //       //password: 'letmein',
+            //       database: 'dev-todo-list'
+            //   },
             };
 
 module.exports = { DEV };
