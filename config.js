a79d031d77a83432791cf4478cf6f2b89e06fb7f
Peter Szujewski <pszujewski@gmail.com>
2017-04-02 10:02:14 -0400

    fixed indentation issue

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 const DEV = {
-              client: 'pg',
-              // ElephantSQL Config
-              connection: process.env.DATABASE_URL || 'postgres://<usernamegoeshere>:<passwordgoeshere>@localhost/todo-app'
-              // Local postgresql server connection Config
-              /*connection: {
-                  user: 'dev',
-                  password: 'letmein',
-                  database: 'dev-todo-list'
-              },*/
-            };
+	client: 'pg',
+	// ElephantSQL Config
+	connection: process.env.DATABASE_URL 
+	//'postgres://<usernamegoeshere>:<passwordgoeshere>@localhost/todo-app'
+	// Local postgresql server connection Config
+	/*connection: {
+		user: 'dev',
+		password: 'letmein',
+		database: 'dev-todo-list'
+	},*/
+};
 
 module.exports = { DEV };
--- server.js
+++ server.js
@@ -19,50 +19,50 @@ app.use(cors());
 app.get('/', (req, res) => {
 	// return all fields except for id
 	knex.select('title', 'completed', 'url', 'order')
-   .from('items')
-   .then(results => {
-  		return res.status(200).json(results);
- 		})
-		.catch(err => { 
-			console.error(err);
-			return res.status(500).json({message: "Internal server error"}); 
-	 });
+	.from('items')
+	.then(results => {
+		return res.status(200).json(results);
+	})
+	.catch(err => { 
+		console.error(err);
+		return res.status(500).json({message: "Internal server error"}); 
+	});
 });
 
 app.get('/:id', (req, res) => {
 	const { id } = req.params;
-  knex.select('title', 'completed', 'url', 'id', 'order')
-   .from('items') 
-   .where('id', id)
-   .then(item => {
-    return res.status(200).json(item[0]);
-   })
-   .catch(err => { 
-			console.error(err);
-			return res.status(500).json({message: "Internal server error"});  
-	 });
+	knex.select('title', 'completed', 'url', 'id', 'order')
+	.from('items') 
+	.where('id', id)
+	.then(item => {
+		return res.status(200).json(item[0]);
+	})
+	.catch(err => { 
+		console.error(err);
+		return res.status(500).json({message: "Internal server error"});  
+	});
 });
 
 app.post('/', jsonParser, (req, res) => {
- 	const { title, order } = req.body;
+	const { title, order } = req.body;
 	const	itemUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
 	// Ensure that the title in the request payload is valid
 	if (title === '' || title === ' '|| typeof title === 'undefined') {
 		return res.status(404).json({message: 'Bad request: enter valid item title'});
 	} 
-  knex.insert({
-  	title: title.trim(),
+	knex.insert({
+		title: title.trim(),
 		order: order,
-  	completed: false,
-  	url: itemUrl
-  })
-  .into('items')
+		completed: false,
+		url: itemUrl
+	})
+	.into('items')
 	.returning(['id', 'title', 'completed', 'url', 'order'])
 	.then(result => {
 		return knex('items')
-  		.where('id', result[result.length - 1].id)
-    	.update({ url: itemUrl.concat(result[result.length - 1].id) })
-    	.returning(['id', 'title', 'completed', 'url', 'order']);
+			.where('id', result[result.length - 1].id)
+			.update({ url: itemUrl.concat(result[result.length - 1].id) })
+			.returning(['id', 'title', 'completed', 'url', 'order']);
 	})
 	.then(output => {
 		console.log(output);
@@ -76,48 +76,48 @@ app.post('/', jsonParser, (req, res) => {
 
 app.delete('/:id', (req, res) => {
 	const {id} = req.params;
-  knex('items')
-  	.where('id', id)
-    .del()
-    .then(result => {
-    	console.log(result);
-      return res.sendStatus(204).send(result);
-    })
-    .catch(err => {
-      console.log(err);
-			return res.status(500).json({message: "Internal server error"}); 
-    });
+	knex('items')
+	.where('id', id)
+	.del()
+	.then(result => {
+		console.log(result);
+		return res.sendStatus(204).send(result);
+	})
+	.catch(err => {
+		console.log(err);
+		return res.status(500).json({message: "Internal server error"}); 
+	});
 });
 
 app.delete('/', (req, res) => {
 	knex('items')
-   .del()
-   .then(result => {
-     return res.status(202).send('delete was successful');
-   })
-	 .catch(err => {
-   	 console.log(err);
-		 return res.status(500).json({message: "Internal server error"}); 
-   });
+	.del()
+	.then(result => {
+		return res.status(202).send('delete was successful');
+	})
+	.catch(err => {
+		console.log(err);
+		return res.status(500).json({message: "Internal server error"}); 
+	});
 });
 
 app.patch('/:id', jsonParser, (req, res) => {
 	const {title, completed, order} = req.body;
-  knex('items')
-   .where('id', req.params.id)
-   .update({title: title, completed: completed, order: order})
-   .then(result => {
-	 	return knex.select('title', 'completed', 'url', 'order')
-    	.from('items')
-    	.where('id', req.params.id)
-		})
-		.then(item => {
-       return res.status(202).json(item[0]);
-    }) 
-    .catch(error => { 
-			console.error(error);
-			return res.status(500).json({message: "Internal server error"}); 
-		});
+	knex('items')
+	.where('id', req.params.id)
+	.update({title: title, completed: completed, order: order})
+	.then(result => {
+		return knex.select('title', 'completed', 'url', 'order')
+			.from('items')
+			.where('id', req.params.id)
+	})
+	.then(item => {
+		return res.status(202).json(item[0]);
+	}) 
+	.catch(error => { 
+		console.error(error);
+		return res.status(500).json({message: "Internal server error"}); 
+	});
 });
 
 app.listen(process.env.PORT || 8080);
